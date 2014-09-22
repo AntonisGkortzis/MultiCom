9a6f4df26d2e72f991533fdac3732e5cbc577bd7
antonisgkortzis <antonis.gkortzis@gmail.com>
2014-09-22 11:01:56 +0200

    Added the MultiComUtilities project

--- Utilities/sharedresources/Message.java
+++ Utilities/sharedresources/Message.java
@@ -8,8 +8,11 @@ public class Message implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private boolean host; //true if the message's destination is a host
 	private boolean command;
 	private String text;
+	private int timestamp;
+	private String user;
 	
 	public Message(){}
 	
